fe5c9bdc82e781a40c8adbe4e7c9f57749d08447
Eric <tedircks@gmail.com>
2019-10-27 11:09:28 -0400

    rename project and sln

---
+++ BacktesterDircksTest/Backtester.Web/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Data
+open System.Linq
+open Microsoft.FSharp.Data
+open Microsoft.FSharp.Linq
+open FSharp.Data
+
+
+[<Literal>]
+let connectionString = 
+    @"Data Source=laptop-8lhpnalu\sqlexpress;Initial Catalog=BackTesterDB;Integrated Security=True"
+do
+    use cmd = new SqlCommandProvider<"
+        INSERT INTO LogTable (LogDate, LogLevel, LogMessage)
+        VALUES (GETDATE(), 'test', 'test')
+        " , connectionString>(connectionString)
+
+    cmd.Execute() |> printfn "%A"
+
+
