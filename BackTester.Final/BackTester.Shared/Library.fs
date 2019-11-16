00276090ef21740c6f17fdd9c68bdffc36b4dc1a
tedircks <tedircks@gmail.com>
2019-11-16 12:29:15 -0500

    backtesterFinal - add shared models for trading and plotting

---
+++ BackTester.Final/BackTester.Shared/Library.fs
@@ -0,0 +1,34 @@
+﻿namespace BackTester.Shared
+open System
+
+// base level models
+type PriceAction = | Open | High | Low | Close | Volume
+type StockData = { Open:float; High:float; Low:float; Close:float; Volume:int }
+
+// container for stock data 
+type StockDataElement = DateTime * StockData
+// list of StockDataElements
+type StockMarketDataList = StockDataElement seq
+
+// container for simple moving average
+type TrendDataElement = DateTime * float
+// list of simple moving average trending data
+type TrendDataList = TrendDataElement seq
+
+// model for simple moving average maintating a squence of period unioned with price action models
+type SimpleMovingAverage = | SimpleMovingAverage of timeSpan:int * price:PriceAction
+with member this.Self = match this with | SimpleMovingAverage (period, price)  -> sprintf "Simple Moving Average %d%A" period price
+
+type Trend = {
+    TrendMarcketDataList : TrendDataList 
+    Stock : string
+    SimpleMovingAverage : SimpleMovingAverage
+} with member this.Self = sprintf "%s(%s)" this.Stock this.SimpleMovingAverage.Self
+
+type Stock = {
+    Ticker : string
+    StockMarketData : StockMarketDataList
+}
+
+// ED : most likely the driver of json parsing
+type StockMarketData = | Stock of Stock | SimpleMovingAverage of Trend
