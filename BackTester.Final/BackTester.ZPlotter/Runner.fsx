9a3f91acd66ee8cc2ca20bdb2f99756d3111de97
tedircks <tedircks@gmail.com>
2019-11-19 16:20:51 -0500

    develop runner script

--- BackTester.Final/BackTester.ZPlotter/Runner.fsx
+++ BackTester.Final/BackTester.ZPlotter/Runner.fsx
@@ -8,14 +8,22 @@
 #r @"\\Mac\Home\Documents\GitHub\gwu\BackTester.Final\BackTester.ZPlotter\bin\Debug\netstandard2.0\BackTester.ZAPI.dll"
 
 open BackTester.Shared
+open BackTester.ZAPI.API
 open BackTester.ZPlotter
-open BackTester.ZAPI
 open BackTester.Trader
 
 let STOCK = "GOOGL"
 let MOVING_AVERAGE_LOW = 100
 let MOVING_AVERAGE_HIGH = 200
+let NUM_STOCKS_TO_TRADE_WHEN_HOLDING = 200
+let NUM_STOCKS_TO_TRADE_WHEN_EMPTY = 100
 
+let GET_TREND_LOW = sprintf "SMA%dClose(%s)" MOVING_AVERAGE_LOW STOCK
+let GET_TREND_HIGH = sprintf "SMA%dClose(%s)" MOVING_AVERAGE_HIGH STOCK
+
+let stockData = GetStockData STOCK
+let movingAverageLow = SimpleMovingAverage (MOVING_AVERAGE_LOW, Close) |> GetTrend STOCK
+let movingAverageHgih = SimpleMovingAverage (MOVING_AVERAGE_HIGH, Close) |> GetTrend STOCK
 
 let MovingAverageStrategy (strategy:IResult) =
     let curTrendLow = strategy.GetTrend(GET_TREND_LOW, 0)
@@ -23,9 +31,35 @@ let MovingAverageStrategy (strategy:IResult) =
     let curTrendHigh = strategy.GetTrend(GET_TREND_HIGH, 0)
     let prevTrendHigh = strategy.GetTrend(GET_TREND_HIGH, -1)
 
-    //TODO : create trading strategy with moving averages
+    match 
+         curTrendLow
+        ,curTrendHigh
+        ,prevTrendLow
+        ,prevTrendHigh
+        with
+          | Some curLowSMA, 
+            Some curHighSMA, 
+            Some prevLowSMA, 
+            Some prevHighSMA ->
+            let NUM_STOCKS_TO_TRADE = 
+                match strategy.GetCurrentPosition(STOCK) 
+                    with 
+                    | Some _ -> NUM_STOCKS_TO_TRADE_WHEN_HOLDING 
+                    | _ -> NUM_STOCKS_TO_TRADE_WHEN_EMPTY
+            match curLowSMA - curHighSMA, prevLowSMA - prevHighSMA 
+                with
+                    | curSMADiff, prevSMADiff when curSMADiff > 0. && prevSMADiff <= 0. -> strategy.ExecuteTrade(STOCK, -NUM_STOCKS_TO_TRADE) |> ignore
+                    | curSMADiff, prevSMADiff when curSMADiff < 0. && prevSMADiff >= 0. -> strategy.ExecuteTrade(STOCK, NUM_STOCKS_TO_TRADE) |> ignore
+                    | _ -> printf "curSMADiff == 0"
+            | _ -> printf "NO TRADE"
 
-    
+let MarketData = 
+    [
+        stockData; 
+        movingAverageLow; 
+        movingAverageHgih
+    ]
+
 let Execute strategy market = 
     {
         MarketData = market; 
@@ -33,6 +67,9 @@ let Execute strategy market =
     } 
     |> Runner.Execute
 
-
-
-// TODO : get results from strategy and pass to plotter
+[
+    MovingAverageStrategy, 
+    MarketData
+] 
+|> List.map (fun market -> market ||> Execute, snd market |> List.toSeq) 
+|> List.map Plotter.Plot 
